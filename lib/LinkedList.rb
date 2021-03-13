6ce8f783f90d3e1c735d04fd9ec9260183beee9e
Richard Denton <richdenton@gmail.com>
2021-03-13 10:00:33 +0000

    Implemented all basic functionality

--- lib/LinkedList.rb
+++ lib/LinkedList.rb
@@ -1,8 +1,10 @@
 require_relative 'Node'
 
 class LinkedList
+  attr_reader :size
   def initialize
     @head = nil
+    @size = 0
   end
 
   def append(value)
@@ -15,6 +17,7 @@ class LinkedList
     else
       @head = Node.new(value)
     end
+    @size += 1
   end
 
   def prepend(value)
@@ -25,34 +28,78 @@ class LinkedList
     else
       @head = Node.new(value)
     end
+    @size += 1
   end
 
   def head
-    @head.value
+    return @head.value if @size > 0
+    nil
   end
 
   def tail
-    tail = nil
     if @head
       tail = @head
       while tail.next_node
         tail = tail.next_node
       end
+    return tail.value
     end
-    tail.value
+    nil
   end
 
-  def size
-    count = 0
-    if @head
-      count += 1
+  def at(index)
+    if index < @size
       node = @head
-      while node.next_node
+      index.times do
         node = node.next_node
-        count += 1
       end
+      node.value
+    else
+      "index out of range"
     end
-  count
+  end
+
+  def pop
+    return nil if @size < 1
+    # Remove head if only one node
+    if @size == 1
+      value = @head.value
+      @head = nil
+      @size -= 1
+      return value
+    end
+    node = @head
+    (@size - 2).times do
+      node = node.next_node
+    end
+    value = node.next_node.value
+    node.next_node = nil
+    @size -= 1
+    value
+  end
+
+  def contains?(value)
+    return false if @size < 1
+    node = @head
+    return true if node.value == value
+    while node.next_node
+      node = node.next_node
+      return true if node.value == value
+    end
+    false
+  end
+
+  def find(value)
+    return nil if size < 1
+    node = @head
+    index = 0
+    return index if node.value == value
+    while node.next_node
+      node = node.next_node
+      index += 1
+      return index if node.value == value
+    end
+    nil
   end
 
   def to_s
--- lib/main.rb
+++ lib/main.rb
@@ -4,8 +4,31 @@ list = LinkedList.new
 list.append(1)
 list.append(2)
 list.append(3)
+list.append(8)
 list.prepend(0)
-puts list.head
-puts list.tail
-puts list.size
+contains = 8
+
+
 puts list.to_s
+puts "head: #{list.head}"
+puts "tail: #{list.tail}"
+puts "Size: #{list.size}"
+puts "At: #{list.at(2)}"
+puts "contains?(#{contains}): #{list.contains?(contains)}"
+puts "find(#{contains}): #{list.find(contains)}"
+puts "Pop: #{list.pop}"
+puts "Size: #{list.size} (after pop)"
+puts list.to_s
+
+list2 = LinkedList.new
+puts "\nTests for an empty list:"
+puts list2.to_s
+puts "head: #{list2.head}"
+puts "tail: #{list2.tail}"
+puts "Size: #{list2.size}"
+puts "At: #{list2.at(2)}"
+puts "contains?(#{contains}): #{list2.contains?(contains)}"
+puts "find(#{contains}): #{list2.find(contains)}"
+puts "Pop: #{list2.pop}"
+puts "Size: #{list2.size} (after pop)"
+puts list2.to_s
